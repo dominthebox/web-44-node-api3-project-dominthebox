8fca4cd92547c6f8bd0304170609b3e5ea602203
dominthebox <dsallustro@gmail.com>
2021-08-05 17:47:22 -0400

    wip, issues connecting things right now

--- api/server.js
+++ api/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const usersRouter = require('./users/users-router');
-
+const { logger } = require('./middleware/middleware');
 const server = express();
+const usersRouter = require('./users/users-router');
 
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json());
 
+server.use(logger)
+
+server.use('./api/users', usersRouter)
 // global middlewares and the user's router need to be connected here
-server.use('./api/users', (req, res, next) => {next(), usersRouter});
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
